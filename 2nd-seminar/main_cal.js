ad3598da65b610028a2ced535f73b0cc02070c1a
namjiyun <namjiyun3275@gmail.com>
2021-11-04 13:09:59 +0900

    Update 2nd-seminar

---
+++ 2nd-seminar/main_cal.js
@@ -0,0 +1,13 @@
+const cal= require("./calculator");
+
+const add=cal.add(1,3);
+console.log(`add result : ${add}`);
+
+const sub=cal.subtract(1,3);
+console.log(`subtract result : ${sub}`);
+
+const mul=cal.multiply(1,3);
+console.log(`multiply result : ${mul}`);
+
+const div=cal.divide(1,3);
+console.log(`divide result : ${div}`);
--- 2nd-seminar/promiseQuiz.js
+++ 2nd-seminar/promiseQuiz.js
@@ -1,6 +1,6 @@
 const members=require("./members")
 
-const getOnline = members => {ss
+const getOnline = members => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const data = members.filter(o => o.location === "online");
@@ -36,5 +36,15 @@ const getOB = members => {
   });
 };
 
+//체이닝
 getOnline(members).then(getOB).then(console.log);
 getYB(members).then(getOffline).then(console.log);
+
+//getOnline(members).then((onlineMembers)=>getOB(OnlineMembers)).then(console.log);
+
+//마지막 부분 async,await으로 변경
+const asyncFunc = async(members)=>{
+  const onlineMembers = await getOnline(members);
+  const offlineMembers = await getOB(onlineMembers);
+  console.log(onlineObMembers)
+}
